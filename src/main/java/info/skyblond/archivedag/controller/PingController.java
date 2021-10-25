64a1895232330ef5373a3bcf85bd101559fa598c
天空Blond <hurui200320@skyblond.info>
2021-10-25 23:05:04 +0800

    Add ping controller. Add lock expire time.
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

---
+++ src/main/java/info/skyblond/archivedag/controller/PingController.java
@@ -0,0 +1,14 @@
+package info.skyblond.archivedag.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+    @GetMapping
+    public String ping() {
+        return "pong!";
+    }
+}
